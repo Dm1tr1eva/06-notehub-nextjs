cd07c6fbca2be1cff9997110c7caed75b1abf48e
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-05-19 16:20:06 +0200

    created header and footer

---
+++ src/lib/api.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import type { Note } from "../types/note";
+
+// ============ KONSTANTEN ============
+const API_BASE = "https://notehub-public.goit.study/api";
+const TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+// ============ AXIOS INSTANCE ============
+const axiosInstance = axios.create({
+  baseURL: API_BASE,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+// ============ TYPES ============
+interface fetchNotesParams {
+  page: number;
+  perPage: number;
+  search?: string;
+}
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export interface CreateNoteRequest {
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
+
+// ============ FUNCTIONS ============
+// ============GET===========
+export async function fetchNotes(
+  params: fetchNotesParams,
+): Promise<FetchNotesResponse> {
+  const response = await axiosInstance.get<FetchNotesResponse>("/notes", {
+    params: {
+      page: params.page,
+      perPage: params.perPage,
+      ...(params.search && { search: params.search }),
+    },
+  });
+
+  return response.data;
+}
+
+// ============POST===========
+export async function createNote(noteData: CreateNoteRequest): Promise<Note> {
+  const response = await axiosInstance.post<Note>("/notes", noteData);
+
+  return response.data;
+}
+
+// ============DELETE===========
+
+export async function deleteNote(noteId: string): Promise<Note> {
+  const response = await axiosInstance.delete<Note>(`/notes/${noteId}`);
+  return response.data;
+}
